a68ce5b3e6e6ba5601dc8a0cd928ca2922c8cf37
skilluence <tech@skilluence.in>
2026-06-13 11:12:16 +0530

    feat: use Skilluence PNG logo via img tag, comment out SVG

--- frontend/src/components/icons/FrappeHRLogo.vue
+++ frontend/src/components/icons/FrappeHRLogo.vue
@@ -1,7 +1,10 @@
 <template>
-	<!-- Skilluence logo (replaces original Frappe HR logo) -->
+	<!-- Skilluence logo (PNG). Original Frappe HR SVG commented out below. -->
+	<img src="/skilluence-logo.png" alt="Skilluence HR" class="h-full w-full object-contain" />
+	<!--
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 117 117" fill="none">
-		<path d="M62 8 H100 a4 4 0 0 1 4 4 V54 a6 6 0 0 1 -2 4 L60 92 a4 4 0 0 1 -6 -3 V52 a6 6 0 0 1 2 -4 L60 11 a4 4 0 0 1 2 -3 Z" fill="#28A745"/>
-		<path d="M55 25 H93 a4 4 0 0 1 4 4 V71 a6 6 0 0 1 -2 4 L53 109 a4 4 0 0 1 -6 -3 V69 a6 6 0 0 1 2 -4 L53 28 a4 4 0 0 1 2 -3 Z" fill="#4285C5"/>
-		</svg>
-		</template>
+	<path d="M62 8 H100 a4 4 0 0 1 4 4 V54 a6 6 0 0 1 -2 4 L60 92 a4 4 0 0 1 -6 -3 V52 a6 6 0 0 1 2 -4 L60 11 a4 4 0 0 1 2 -3 Z" fill="#28A745"/>
+	<path d="M55 25 H93 a4 4 0 0 1 4 4 V71 a6 6 0 0 1 -2 4 L53 109 a4 4 0 0 1 -6 -3 V69 a6 6 0 0 1 2 -4 L53 28 a4 4 0 0 1 2 -3 Z" fill="#4285C5"/>
+	</svg>
+	-->
+	</template>
